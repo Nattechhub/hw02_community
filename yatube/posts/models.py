3b279e3cea64fd06a390e15d0f94f36fb789bfca
Анатолий Черножуков <sir.kalashnik@yandex.ru>
2023-01-24 05:24:38 +0400

    Финальный проект спринта 3

---
+++ yatube/posts/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Группа"
+    )
+    slug = models.SlugField(unique=True)
+    description = models.TextField(
+        max_length=400,
+        verbose_name="Описание"
+    )
+    
+    def _str_(self):
+        return self.title
+class Post(models.Model):
+    text = models.TextField(verbose_name="Содержание")
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата"
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    ) 
+
+    def _str_(self):
+        return self.text
+
