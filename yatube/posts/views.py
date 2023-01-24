d8c6b9de42992bf68b089ebe5536cbe7465236c3
Анатолий Черножуков <sir.kalashnik@yandex.ru>
2023-01-24 06:19:01 +0400

    Поправил flake8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,15 +7,15 @@ def index(request):
     template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {'posts': posts}
-    return render(request, template, context) 
+    return render(request, template, context)
 
 
-def group_posts(request, slug):    
-     template = 'posts/group_list.html'
-     group = get_object_or_404(Group, slug=slug)
-     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-     context = {
-         'group': group,
-         'posts': posts,
-     }
-     return render(request, template, context) 
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
