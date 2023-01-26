b16238d0526a1e60221d808f38988e8328e32b74
Анатолий Черножуков <sir.kalashnik@yandex.ru>
2023-01-26 17:16:56 +0400

    Вынес кол-во постов в отдельную константу. Поправил select_related

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,16 +4,18 @@ from .models import Post, Group
 
 
 def index(request):
+    post_per_page = 10
     template = 'posts/index.html'
-    posts = Post.objects.select_related('group').order_by('-pub_date')[:10]
+    posts = Post.objects.all().order_by('-pub_date')[:post_per_page]
     context = {'posts': posts}
     return render(request, template, context)
 
 
 def group_posts(request, slug):
+    post_per_page = 10
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all().order_by('-pub_date')[:10]
+    posts = group.posts.all().order_by('-pub_date')[:post_per_page]
     context = {
         'group': group,
         'posts': posts,
